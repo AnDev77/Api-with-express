4b1f8d02d9676702121bcfbfec483cdf93aa1756
AnDev77 <dnr0627@naver.com>
2024-09-04 14:04:02 +0900

    ~9/5 study log

--- youtuber-demo.js
+++ youtuber-demo.js
@@ -35,7 +35,7 @@ app.get('/youtubers/:id', (req, res) => {
     let {id} = req.params
     id = parseInt(id)
     const youtuber = db.get(id) 
-    if (!(id in db.keys())){
+    if (db.get(id) == undefined){
         res.json({
             message : '해당 유튜버를 찾지 못했습니다.'
         })
@@ -62,10 +62,65 @@ app.post('/youtuber', (req, res)=>{
         subscriber : subscriber,
         videoNum : videoNum
     }
-    let id = db.size() + 1
-    db.set(id , req.body)
+    db.set(id++ , req.body)
     res.json({
-        message: `${db.get(id).channelTitle} 님의 유튜버 생활을 응원합니다.`
+        message: `${db.get(id-1).channelTitle} 님의 유튜버 생활을 응원합니다.`
     })
+    console.log(db)
 
+})
+
+app.delete('/youtubers/:id', (req, res) => {
+
+    let {id} = req.params
+    id = parseInt(id)
+    if (!(db.get(id) == undefined)){
+        let name = db.get(id).channelTitle
+        db.delete(id)
+        res.json({
+            message : `${name} 님의 계정을 삭제했습니다.`
+        })
+    } else {
+        res.json({
+            message : `id 가 ${id}인 유튜버를 찾을 수 없습니다.`
+        })
+    }    
+
+})
+
+app.delete('/youtubers', (req, res) => {
+    
+    
+    if (db.size > 0) {
+        for (let key of db.keys()){
+        db.delete(key)
+        }
+        res.json({
+            message : '모든 유튜버가 삭제 됐습니다.'
+        })
+    
+    } else{
+        res.json({
+            message : '삭제할 유튜버가 없습니다.'
+        })
+    }
+})
+
+app.put('/youtubers/:id', (req, res) => {
+    let {id} = req.params
+    const newTitle = req.body.channelTitle
+    id = parseInt(id)
+    let tempYoutuber = db.get(id)
+    if (tempYoutuber == undefined){
+        res.json({
+            message : "해당 유튜버를 찾을 수 없습니다."
+        })
+    } else {
+        xTitle = db.get(id).channelTitle
+        tempYoutuber.channelTitle = newTitle
+        db.set(id, tempYoutuber)
+        res.json({
+            massge : `${xTitle} 님의 채널명이 ${newTitle}로 변경 되었습니다.`
+        })
+        }
 })
